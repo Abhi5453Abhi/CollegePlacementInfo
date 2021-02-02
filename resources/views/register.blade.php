9e9e8b24e7488c32bdc66e8da076dc3e7b8f529b
Abhishek <abhishek@192.168.1.8>
2021-02-02 10:14:16 +0530

    Fixing Validations

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,17 +9,17 @@
       <div class="form-group">
          <label>Name</label>
          <input type="text" name="name" placeholder="Enter name"> <br>
-         <span style="color: red">@error('name'){{$message}}</span>
+         <span style="color: red">@error('name'){{$message}}@enderror</span>
       </div>
       <div class="form-group">
          <label>Email</label>
          <input type="text" name="email" placeholder="Enter email"><br>
-         <span style="color: red">@error('email'){{$message}}</span>
+         <span style="color: red">@error('email'){{$message}}@enderror</span>
       </div>
       <div class="form-group">
          <label>Password</label>
          <input type="text" name="password" placeholder="Enter password"><br>
-         <span style="color: red">@error('password'){{$message}}</span>
+         <span style="color: red">@error('password'){{$message}}@enderror</span>
       </div>
       <button type="submit" class="btn btn-primary">Register</button>
    </form>
