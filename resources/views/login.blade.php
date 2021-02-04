0395b53211eed89f4d995318af405cf0d9aaba5b
Abhishek <abhishek@192.168.1.3>
2021-02-04 11:54:26 +0530

    Middleware

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
 
-@if(Session::get('status'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  {{Session::get('status')}}
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
-@else
 <h1>Login Page</h1>
 <div class="col-sm-6">
    <form method="POST" action="login">
@@ -27,6 +19,5 @@
       <button type="submit" class="btn btn-primary">Login</button>
    </form>
 </div>
-@endif
 
 @stop
